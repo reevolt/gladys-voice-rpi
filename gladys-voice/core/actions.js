2fd57754cb3144d78ea0988bac40cf8cfadcfec2
Romain Cherix <rcherix@groupemutuel.ch>
2018-02-23 08:07:31 +0100

    add scenes

--- gladys-voice/core/actions.js
+++ gladys-voice/core/actions.js
@@ -12,12 +12,50 @@ module.exports = {
         client.post("http://192.168.1.134:3000/write/CORNICHE_SWITCH?value=0", function(){});
     },
     monterStore : function() {
-        client.post("http://192.168.1.134:3000/write/STORES_SEJOUR?value=false", function(){});
-        client.post("http://192.168.1.134:3000/write/STORES_CUISINE?value=false", function(){});
+        var args = {
+            data: [
+                { 'address': 'STORES_SEJOUR', 'value': false },
+                { 'address': 'STORES_CUISINE', 'value': false }
+            ],
+            headers: { "Content-Type": "application/json" }
+        };
+        client.post("http://192.168.1.134:3000/scene", args, function(){});
     },
     baisserStore : function() {
-        client.post("http://192.168.1.134:3000/write/STORES_SEJOUR?value=true", function(){});
-        client.post("http://192.168.1.134:3000/write/STORES_CUISINE?value=true", function(){});
+        var args = {
+            data: [
+                { 'address': 'STORES_SEJOUR', 'value': true },
+                { 'address': 'STORES_CUISINE', 'value': true }
+            ],
+            headers: { "Content-Type": "application/json" }
+        };
+        client.post("http://192.168.1.134:3000/scene", args, function(){});
+    },
+    bonneNuit:  function() {
+        var args = {
+            data: [
+                { 'address': 'STORES_SEJOUR', 'value': false },
+                { 'address': 'STORES_CUISINE', 'value': false },
+                { 'address': 'CUISINE_SWITCH', 'value': false },
+                { 'address': 'CUISINE_LED_SWITCH', 'value': false },
+                { 'address': 'CORNICHE_SWITCH', 'value': false },
+                { 'address': 'SALLEMANGER_SWITCH', 'value': false },
+                { 'address': 'STORES_PARENTS', 'value': true },
+            ],
+            headers: { "Content-Type": "application/json" }
+        };
+        client.post("http://192.168.1.134:3000/scene", args, function(){});
+    },
+    hello:  function() {
+        var args = {
+            data: [
+                { 'address': 'CUISINE_DIMMING', 'value': 30 },
+                { 'address': 'CUISINE_LED_DIMMING', 'value': 30 },
+                { 'address': 'CORNICHE_DIMMING', 'value': 30 },
+            ],
+            headers: { "Content-Type": "application/json" }
+        };
+        client.post("http://192.168.1.134:3000/scene", args, function(){});
     },
 }
 
